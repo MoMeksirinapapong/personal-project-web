9bf0a341a6983522eb8f56edc9c3044fa0d3310e
Momeksirinapapong <ms.supapornn@gmail.com>
2023-03-14 16:33:47 +0700

    Frontend

--- src/pages/resultPage.js
+++ src/pages/resultPage.js
@@ -1,17 +1,42 @@
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Dark from "../assets/dark.jpg";
 
 export default function ResultPage() {
-  const [addNote, setAddnote] = useState([]);
+  const [addNote, setAddnote] = useState("Please input Note");
   const [newNote, setNewnote] = useState("");
+  const [update, setUpdate] = useState(false);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setAddnote([...addNote, newNote]);
-    setNewnote("");
-  }
+  const getUser = async () => {
+    const res = await axios.get("http://localhost:8888/auth/user");
+    // console.log(res.data);
+    setAddnote(res.data.user.note);
+  };
+
+  const postNote = async (aaa) => {
+    const res = await axios.patch("http://localhost:8888/auth/note", {
+      note: aaa,
+    });
+    // console.log(res.data);
+    // setAddnote(res.data.user.note);
+  };
+
+  useEffect(() => {
+    getUser();
+    return () => setUpdate(false);
+  }, [update]);
+
+  console.log("addNote", addNote);
+  //Add note
+  const handleSubmit = async (e) => {
+    // e.preventDefault();
+    // await setAddnote([...addNote, newNote]);
+    // setNewnote("");
+    postNote(newNote);
+    setUpdate(true);
+  };
 
   function handleChange(e) {
     setNewnote(e.target.value);
@@ -25,15 +50,13 @@ export default function ResultPage() {
           <ArrowBackIcon color="white" fontSize="large" />
         </Link>
       </div>
-      <div className="font-serif text-[30px] text-white absolute top-0 mt-20 ml-52 z-10">
+      <div className="font-serif text-[30px] text-white absolute top-0 mt-20 ml-44 z-10">
         Note
       </div>
-
-      <div className="absolute top-36 text-white font-serif text-[18px] ml-24 ">
+      {/* New Note */}
+      <div className="absolute top-36 text-white font-serif text-[18px] ml-20 w-[250px] h-[200px]">
         <ul>
-          {addNote.map((note, index) => (
-            <li key={index}>{note}</li>
-          ))}
+          <p className=" break-words overflow-auto ">{addNote}</p>
         </ul>
         <form onSubmit={handleSubmit}>
           <label className="text-yellow-400" htmlFor="note">
@@ -41,7 +64,7 @@ export default function ResultPage() {
           </label>
           <br />
           <textarea
-            className="text-black"
+            className="text-black break-words  overflow-auto w-[250px] h-[200px] "
             id="note"
             value={newNote}
             onChange={handleChange}
@@ -50,18 +73,15 @@ export default function ResultPage() {
           <button
             className="border hover:bg-gray-200 hover:text-black"
             type="submit"
+            onClick={() => handleSubmit()}
           >
             Submit
           </button>
+
           <button
-            className="border hover:bg-gray-200 hover:text-black ml-4"
+            className="border hover:bg-gray-200 hover:text-black ml-4 "
             type="submit"
-          >
-            Edit
-          </button>
-          <button
-            className="border hover:bg-gray-200 hover:text-black ml-4"
-            type="submit"
+            onClick={() => postNote("Please input Note")}
           >
             Delete
           </button>
